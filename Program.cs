b62d69a65365b463c4a79a7b61d116de307bebaf
Tahiel24 <equi27@live.com.ar>
2022-08-26 18:57:01 -0300

    Punto 2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿//Punto 1
-Console.WriteLine("Ingrese un numero para calcular su cuadrado:");
-try{
-var num=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("El resultado es: "+ (num*num));
+﻿//Punto 2
+Console.WriteLine("Ingrese un numero: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese otro numero: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+try
+{
+    int resultado = num1 / num2;
+
+    Console.WriteLine("Resultado: " + resultado);
 }
-catch(FormatException){
-    Console.WriteLine("ERROR: Se ha ingresado un caracter erroneo");
-}
-catch(OverflowException){
-    Console.WriteLine("ERROR: Se ha excedido el rango del tipo de dato declarado");
-}
-catch(Exception ex){
-    throw ex;
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+    //Console.WriteLine("Division por cero erronea");
 }
 
