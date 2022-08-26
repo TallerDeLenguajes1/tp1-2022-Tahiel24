d7da8a0727317a57acd2ffd2dd7645b7d999b9d8
Tahiel24 <equi27@live.com.ar>
2022-08-26 18:18:13 -0300

    Punto 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Punto 1
+Console.WriteLine("Ingrese un numero para calcular su cuadrado:");
+try{
+var num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("El resultado es: "+ (num*num));
+}
+catch(FormatException){
+    Console.WriteLine("ERROR: Se ha ingresado un caracter erroneo");
+}
+catch(OverflowException){
+    Console.WriteLine("ERROR: Se ha excedido el rango del tipo de dato declarado");
+}
+catch(Exception ex){
+    throw ex;
+}
+
