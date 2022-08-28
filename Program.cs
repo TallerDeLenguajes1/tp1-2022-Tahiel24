9e46b76e6714d2e14adbf33ef8e899cd0fa09435
Tahiel24 <tahielvarela@gmail.com>
2022-08-28 12:38:11 -0300

    Punto 2-4 terminado

--- Program.cs
+++ Program.cs
@@ -1,16 +1,34 @@
-﻿//Punto 1
-Console.WriteLine("Ingrese un numero para calcular su cuadrado:");
-try{
-var num=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("El resultado es: "+ (num*num));
+﻿//Punto 4 
+using System.Net;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+
+
+string url = @"https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre ";
+var request = (HttpWebRequest)WebRequest.Create(url);
+request.Method = "GET";
+request.ContentType = "application/json";
+request.Accept = "application/json";
+Root enlistarDatos;
+try
+{
+    WebResponse response = request.GetResponse();
+    Stream strReader = response.GetResponseStream();
+    if (strReader == null) return;
+    StreamReader objReader = new StreamReader(strReader);
+    string responseBody = objReader.ReadToEnd();
+    enlistarDatos = JsonSerializer.Deserialize<Root>(responseBody);
+    //Usar datos obtenidos: 
+    Console.WriteLine("Listado de provincias y su correspondiente id: ");
+    foreach(var i in enlistarDatos.Provincias){
+        Console.WriteLine("Nombre: "+i.Nombre);
+        Console.WriteLine("ID: "+i.Id+"\n");
+    }
 }
-catch(FormatException){
-    Console.WriteLine("ERROR: Se ha ingresado un caracter erroneo");
-}
-catch(OverflowException){
-    Console.WriteLine("ERROR: Se ha excedido el rango del tipo de dato declarado");
-}
-catch(Exception ex){
-    throw ex;
+catch (WebException ex)
+{
+    Console.WriteLine("Error: "+ex.Message);
 }
 
